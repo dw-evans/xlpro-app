e7ec7c303bbe2fa000c8867c7171c7bb01f3559d
dw-evans <106231868+dw-evans@users.noreply.github.com>
2025-03-18 22:46:46 +0000

    type hint add-in support tested. Workflow would be to generate the cs files, compile and reload the addin. Next step is to get xlpro working on a per-environment basis. Write the lockfile to the env/,,,/.xlpro directory ...

---
+++ xlpro/xll/xlproxll/Class1.cs
@@ -0,0 +1,20 @@
+﻿using ExcelDna.Integration;
+
+public static class MyFunctions
+{
+    [ExcelFunction(Description = "My first .NET function")]
+    public static string SayHello(string name)
+    {
+        return "Hello " + name;
+    }
+
+    [ExcelFunction(Description = "A useful test function that adds two numbers, and returns the sum.")]
+    public static double AddThem(
+        [ExcelArgument(Name = "Augend", Description = "is the first number, to which will be added")] 
+        double v1,
+        [ExcelArgument(Name = "Addend", Description = "is the second number that will be added")]     
+        double v2)
+    {
+        return v1 + v2;
+    }
+}
---
+++ xlpro/xll/xlproxll/Class2.cs
@@ -0,0 +1,14 @@
+using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+
+public class IntelliSenseAddIn : IExcelAddIn
+{
+    public void AutoOpen()
+    {
+        IntelliSenseServer.Install();
+    }
+    public void AutoClose()
+    {
+        IntelliSenseServer.Uninstall();
+    }   
+}
