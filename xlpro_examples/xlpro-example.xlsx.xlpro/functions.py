fe25b9e43d3854308b902b54c4030cbc61930220
dw-evans <106231868+dw-evans@users.noreply.github.com>
2025-06-21 15:06:59 +0100

    example function development

---
+++ xlpro_examples/xlpro-example.xlsx.xlpro/functions.py
@@ -0,0 +1,101 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+plt.style.use('default')
+import numpy as np
+import matplotlib.figure
+
+# from win32typelibs import excel as xl
+
+import numpy as np
+
+import xlpro
+import xlpro._utils as utils
+from xlpro import list1d, list2d, ndarray1d, ndarray2d
+
+import pandas as pd
+
+xlpro.register()(xlpro.show)
+xlpro.register()(xlpro.show_image)
+xlpro.register()(xlpro.show_image_with_seed)
+xlpro.register()(xlpro.pytype)
+xlpro.register()(xlpro.cpy)
+xlpro.register()(xlpro.deepcpy)
+xlpro.register()(xlpro.pow)
+xlpro.register()(xlpro.mul)
+xlpro.register()(xlpro.div)
+xlpro.register()(xlpro.add)
+xlpro.register()(xlpro.subtract)
+xlpro.register()(xlpro.pyhash)
+
+
+def np_linspace(
+    start,
+    stop,
+    num: int = 50,
+    endpoint: bool = True,
+    retstep: bool = False,
+    dtype: None = None,
+    axis:int = 0,
+    device:str = "cpu"
+):
+    return np.linspace(
+        start=start,
+        stop=stop,
+        num=num,
+        endpoint=endpoint,
+        retstep=retstep,
+        dtype=dtype,
+        axis=axis,
+        device=device
+    )
+
+@xlpro.comsafe
+def visualise_color(rgb:ndarray1d[int], caller:'xl.Range'):
+    rgb = rgb[0]
+    caller.Interior.Color = utils.rgb2int(rgb)
+    return str(rgb)
+
+def mpl_create_figure_with_seed(seed) -> matplotlib.figure.Figure:
+    fig, ax = plt.subplots()
+    fig.tight_layout()
+    return fig
+
+def mpl_get_axes_of_fig(fig:matplotlib.figure.Figure):
+    return list(fig.get_axes())
+
+def mpl_add_line_unique(
+    ax:matplotlib.axes.Axes, 
+    x:ndarray1d, 
+    y:ndarray1d, 
+    tag:str, 
+    fmt:str=None,
+    color:list1d=None, 
+    linewidth=2,
+    markersize=12,
+):
+    if tag is None:
+        raise Exception(f"InvalidTag")
+    for line in ax.get_lines():
+        if tag == getattr(line, "_tag", None):
+            line.remove()
+    ln, = ax.plot(x, y, color=color, linewidth=linewidth, markersize=markersize)
+    ln._tag = tag
+    return ln
+
+def mpl_add_legend(ax:matplotlib.axes.Axes, handles:list1d, labels:list1d):
+    legend = ax.legend(
+        labels=labels,
+        handles=handles,
+        labelspacing=1.0,        # Line spacing
+        fontsize=9.0,            # Text size
+        frameon=False            # No border or background
+    )
+
+    return legend
+
+
+
+
+
