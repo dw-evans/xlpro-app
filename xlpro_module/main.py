8d6acbee3104bdffb174b8a28266708593e4d64b
dw-evans <106231868+dw-evans@users.noreply.github.com>
2025-10-17 17:01:48 +0100

    Removed xlpro subfolder in preparation for submodule-ing

--- xlpro_module/main.py
+++
@@ -1,9 +0,0 @@
-import xlpro.run_server
-
-
-def main():
-    xlpro.run_server.main()
-
-
-if __name__ == "__main__":
-    main()
--- xlpro_module/xlpro/_enums.py
+++
@@ -1,9 +0,0 @@
-class FunctionTypes:
-    array_or_value = 0
-    py_object = 1
-
-    @classmethod
-    def as_list(cls):
-        return [value for key, value in vars(cls).items() if isinstance(value, int)]
-    
-
